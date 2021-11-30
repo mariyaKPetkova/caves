d9bda09442fd4fa7e969a027d31cf09fb6f2e88c
mariyaKPetkova <90306706+mariyaKPetkova@users.noreply.github.com>
2021-11-30 22:31:43 +0200

    add css

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -6,34 +6,34 @@ const Create = () => {
                     <legend>Add new Cave</legend>
                     <p className="field">
                         <label htmlF
-                        or="name">Name</label>
+                        or="name">Name: </label>
                         <span className="input">
                             <input type="text" name="name" id="name" placeholder="Name"/>
                         </span>
                     </p>
                     <p className="field">
                         <label htmlF
-                        or="location">Location</label>
+                        or="location">Location: </label>
                         <span className="input">
                             <input type="text" name="location" id="location" value="Location"/>
                         </span>
                     </p>
                     <p className="field">
                         <label htmlF
-                        or="description">Description</label>
+                        or="description">Description: </label>
                         <span className="input">
                             <textarea name="description" id="description" placeholder="Description"></textarea>
                         </span>
                     </p>
                     <p className="field">
                         <label htmlF
-                        or="image">Image</label>
+                        or="image">Image: </label>
                         <span className="input">
                             <input type="text" name="imageUrl" id="image" placeholder="Image"/>
                         </span>
                     </p>
                     
-                    <input className="button submit" type="submit" value="Add Cave"/>
+                    <input className="button-submit" type="submit" value="Add Cave"/>
                 </fieldset>
             </form>
         </section>
--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -6,21 +6,21 @@ const Edit = () => {
                     <legend>Edit my Cave</legend>
                     <p className="field">
                         <label htmlF
-                        or="name">Name</label>
+                        or="name">Name: </label>
                         <span className="input">
                             <input type="text" name="name" id="name" value=""/>
                         </span>
                     </p>
                     <p className="field">
                         <label htmlF
-                        or="location">Location</label>
+                        or="location">Location: </label>
                         <span className="input">
                             <input type="text" name="location" id="location" value=""/>
                         </span>
                     </p>
                     <p className="field">
                         <label htmlF
-                        or="description">Description</label>
+                        or="description">Description: </label>
                         <span className="input">
                             <textarea name="description"
                                 id="description">some description</textarea>
@@ -28,13 +28,13 @@ const Edit = () => {
                     </p>
                     <p className="field">
                         <label htmlF
-                        or="image">Image</label>
+                        or="image">Image: </label>
                         <span className="input">
                             <input type="text" name="imageUrl" id="image" value="/images/dog.png"/>
                         </span>
                     </p>
                     
-                    <input className="button submit" type="submit" value="Save"/>
+                    <input className="button-submit" type="submit" value="Save"/>
                 </fieldset>
             </form>
         </section>
