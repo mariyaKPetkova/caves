e6e772036580c71dd51191f0f77970dc2e554670
mariyaKPetkova <90306706+mariyaKPetkova@users.noreply.github.com>
2022-02-08 15:08:19 +0200

    change html and css

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -4,10 +4,10 @@ import * as caveService from '../../services/caveService.js'
 import { useAuthContext } from '../../contexts/AuthContext.js'
 
 const Create = () => {
-     const navigate = useNavigate()
-    const {user} = useAuthContext()
-    const [errors,setErrors] = useState({name:false,location:false,description:false,imageUrl:false})
-    const onCreate = (e)=>{
+    const navigate = useNavigate()
+    const { user } = useAuthContext()
+    const [errors, setErrors] = useState({ name: false, location: false, description: false, imageUrl: false })
+    const onCreate = (e) => {
         e.preventDefault()
         const form = new FormData(e.currentTarget)
 
@@ -21,84 +21,77 @@ const Create = () => {
             location,
             description,
             imageUrl
-        },user.accessToken)
-        .then(result=>{
-            navigate('/')
-        })
+        }, user.accessToken)
+            .then(result => {
+                navigate('/')
+            })
     }
-    const onName = (e) =>{
+    const onName = (e) => {
         const curr = e.currentTarget.value
-        if(curr.length < 3){
-            setErrors(state =>({...state, name:'Name must be at least 3 characters long'}))
-        }else if(curr.length > 20){
-            setErrors(state =>({...state, name:'Name must be maximum 20 characters long'}))
-        }else{
-            setErrors(state =>({...state, name:false}))
+        if (curr.length < 3) {
+            setErrors(state => ({ ...state, name: 'Name must be at least 3 characters long' }))
+        } else if (curr.length > 20) {
+            setErrors(state => ({ ...state, name: 'Name must be maximum 20 characters long' }))
+        } else {
+            setErrors(state => ({ ...state, name: false }))
         }
     }
-    const onLocation = (e) =>{
+    const onLocation = (e) => {
         const curr = e.currentTarget.value
-        if(curr.length < 3){
-            setErrors(state =>({...state, location:'Location must be at least 3 characters long'}))
-        }else if(curr.length > 25){
-            setErrors(state =>({...state, location:'Location must be maximum 25 characters long'}))
-        }else{
-            setErrors(state =>({...state, location:false}))
+        if (curr.length < 3) {
+            setErrors(state => ({ ...state, location: 'Location must be at least 3 characters long' }))
+        } else if (curr.length > 25) {
+            setErrors(state => ({ ...state, location: 'Location must be maximum 25 characters long' }))
+        } else {
+            setErrors(state => ({ ...state, location: false }))
         }
     }
-    const onDescription = (e) =>{
+    const onDescription = (e) => {
         const curr = e.currentTarget.value
-        if(curr.length < 3){
-            setErrors(state =>({...state, description:'Description must be at least 3 characters long'}))
-        }else if(curr.length > 150){
-            setErrors(state =>({...state, description:'Description must be maximum 150 characters long'}))
-        }else{
-            setErrors(state =>({...state, description:false}))
+        if (curr.length < 3) {
+            setErrors(state => ({ ...state, description: 'Description must be at least 3 characters long' }))
+        } else if (curr.length > 150) {
+            setErrors(state => ({ ...state, description: 'Description must be maximum 150 characters long' }))
+        } else {
+            setErrors(state => ({ ...state, description: false }))
         }
     }
-    const onImageUrl = (e) =>{
+    const onImageUrl = (e) => {
         const curr = e.currentTarget.value
-        if(curr.length < 1){
-            setErrors(state =>({...state, imageUrl:'Image is required'}))
-        }else{
-            setErrors(state =>({...state, imageUrl:false}))
+        if (curr.length < 1) {
+            setErrors(state => ({ ...state, imageUrl: 'Image is required' }))
+        } else {
+            setErrors(state => ({ ...state, imageUrl: false }))
         }
     }
-    return(
+    return (
         <section id="create-page" className="create">
             <form id="create-form" onSubmit={onCreate} method="Post">
                 <fieldset>
                     <legend>Add new Cave</legend>
-                    <p className="field">
-                        <label>Name: </label>
-                        <p className="field">
-                            <input type="text" name="name" id="name" onChange={onName} />
-                        </p>
-                        <span style={{display: errors.name?'inline':'hidden'}}>{errors.name}</span>
-                    </p>
-                    <p className="field">
-                        <label >Location: </label>
-                        <p className="field">
-                            <input type="text" name="location" id="location" onChange={onLocation} />
-                        </p>
-                        <span style={{display: errors.location?'inline':'hidden'}}>{errors.location}</span>
-                    </p>
-                    <p className="field">
-                        <label >Description: </label>
-                        <p className="field">
-                            <textarea name="description" id="description" onChange={onDescription} />
-                        </p>
-                        <span style={{display: errors.description?'inline':'hidden'}}>{errors.description}</span>
-                    </p>
-                    <p className="field">
-                        <label>Image: </label>
-                        <p className="field">
-                            <input type="text" name="imageUrl" id="image" onChange={onImageUrl} />
-                        </p>
-                        <span style={{display: errors.imageUrl?'inline':'hidden'}}>{errors.imageUrl}</span>
-                    </p>
-                    
-                    <input className="button-submit" type="submit" value="Add Cave"/>
+
+                    <label>Name: </label>
+                    <input type="text" name="name" id="name" onChange={onName} />
+                    <span style={{ display: errors.name ? 'inline' : 'hidden' }}>{errors.name}</span>
+
+
+                    <label >Location: </label>
+                    <input type="text" name="location" id="location" onChange={onLocation} />
+                    <span style={{ display: errors.location ? 'inline' : 'hidden' }}>{errors.location}</span>
+
+
+                    <label >Description: </label>
+                    <textarea name="description" id="description" onChange={onDescription} />
+                    <span style={{ display: errors.description ? 'inline' : 'hidden' }}>{errors.description}</span>
+
+
+                    <label>Image: </label>
+                    <input type="text" name="imageUrl" id="image" onChange={onImageUrl} />
+                    <span style={{ display: errors.imageUrl ? 'inline' : 'hidden' }}>{errors.imageUrl}</span>
+
+
+                    <input className="button-submit" type="submit" value="Add Cave" />
+
                 </fieldset>
             </form>
         </section>
